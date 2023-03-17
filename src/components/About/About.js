3fa8b66424b2b854e17811f62ef0b4930a4a1853
dkrj45 <dkrj45@gmail.com>
2023-03-17 18:52:32 -0400

    header mobile menu toggler design changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,50 +1,68 @@
-import './About.scss';
-import resume from '../../assets/files/resume-chris-lee.pdf';
+import "./About.scss";
+import resume from "../../assets/files/resume-chris-lee.pdf";
 
 function About() {
-
-    return (
-        <div id='about' className='about'>
-            <h2 className='about__title'>About Me</h2>
-            <p className='about__subtitle'>Ex-public employee with background in engineering</p>
-            <div className='about__tablet-aligner'>
-                <div className='about__tablet-divider'>
-                    <div className='about__skills-container'>
-                        <h3 className='about__skills-title'>Skills</h3>
-                        <ul className='about__skills-list'>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>Javascript</li>
-                            <li>React</li>
-                            <li>Node</li>
-                            <li>Express</li>
-                            <li>MySQL</li>
-                            <li>Sass</li>
-                            <li>DOM APIs</li>
-                            <li>Web APIs</li>
-                            <li>User Authentication</li>
-                            <li>OAuth</li>
-                            <li>AWS</li>
-                            <li>Heroku</li>
-                            <li>Git/Github</li>
-                            <li>Jest</li>
-                            <li>Figma</li>
-                            <li>Object Oriented Programming</li>
-                            <li>Responsive Design</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p className='about__text'>A technology and engineering enthusiast making a career transition into Web Development. I enjoy bringing ideas to life with creativity, aesthetics and functionality in mind.</p>
-                        <p className='about__text'>With previous background in mechanical engineering and investigative services comes a proven record of thinking like a machine and problem solving.</p>
-                        <p className='about__text'>Check out the 'Projects' section below to see my latest deployed websites!</p>
-                        <div className='about__resume-container'>
-                            <a href={resume} download='chris-lee-resume.pdf' className='about__resume'>Download Resume</a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div id="about" className="about">
+      <h2 className="about__title">About Me</h2>
+      <p className="about__subtitle">
+        Ex-public employee with background in engineering
+      </p>
+      <div className="about__tablet-aligner">
+        <div className="about__tablet-divider">
+          <div className="about__skills-container">
+            <h3 className="about__skills-title">Skills</h3>
+            <ul className="about__skills-list">
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>React</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>PostgreSQL</li>
+              <li>Sass</li>
+              <li>DOM APIs</li>
+              <li>Web APIs</li>
+              <li>User Authentication</li>
+              <li>OAuth</li>
+              <li>AWS</li>
+              <li>Heroku</li>
+              <li>Git/Github/Gitlab</li>
+              <li>Socket.io</li>
+              <li>Figma</li>
+              <li>Object Oriented Programming</li>
+              <li>Responsive Design</li>
+            </ul>
+          </div>
+          <div>
+            <p className="about__text">
+              A technology and engineering enthusiast making a career transition
+              into Web Development. I enjoy bringing ideas to life with
+              creativity, aesthetics and functionality in mind.
+            </p>
+            <p className="about__text">
+              With previous background in mechanical engineering and
+              investigative services comes a proven record of thinking like a
+              machine and problem solving.
+            </p>
+            <p className="about__text">
+              Check out the 'Projects' section below to see my latest deployed
+              websites!
+            </p>
+            <div className="about__resume-container">
+              <a
+                href={resume}
+                download="chris-lee-resume.pdf"
+                className="about__resume"
+              >
+                Download Resume
+              </a>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,105 @@
-import './Header.scss';
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import user from '../../assets/icons/user.png';
-import folder from '../../assets/icons/folder.png';
-import send from '../../assets/icons/send.png';
-import menu from '../../assets/icons/menu.png';
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import user from "../../assets/icons/user.png";
+import folder from "../../assets/icons/folder.png";
+import send from "../../assets/icons/send.png";
+import menu from "../../assets/icons/menu.png";
+import x from "../../assets/icons/x.png";
 
 function App() {
-
-  let [toggler, setToggler] = useState(false)
+  let [toggler, setToggler] = useState(false);
 
   const toggleNav = () => {
     if (!toggler) {
-      setToggler(true)
+      setToggler(true);
     } else {
-      setToggler(false)
+      setToggler(false);
     }
-  }
+  };
 
   return (
-    <div className='header'>
-      <nav className='nav'>
-        <Link to="/Home" className='nav__logo'>lee</Link>
-        <div>
-          <div className='nav__options'>
-            <a href='#about' className='nav__option-container'><img src={user} alt='user-icon'/><h2 href='about' className='nav__option'>About</h2></a>
-            <a href='#projects'className='nav__option-container'><img src={folder} alt='folder-icon' /><h2 href='#projects' className='nav__option'>Projects</h2></a>
-            <a href='#contact' className='nav__option-container'><img src={send} alt='send-icon' /><h2 href='#contact' className='nav__option'>Contact</h2></a>
+    <>
+      {toggler ? <div onClick={toggleNav} className="not-header"></div> : ""}
+      <div
+        className="header"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <nav className="nav">
+          <Link to="/Home" className="nav__logo">
+            lee
+          </Link>
+          <div>
+            <div className="nav__options">
+              <a href="#about" className="nav__option-container">
+                <img src={user} alt="user-icon" />
+                <h2 href="about" className="nav__option">
+                  About
+                </h2>
+              </a>
+              <a href="#projects" className="nav__option-container">
+                <img src={folder} alt="folder-icon" />
+                <h2 href="#projects" className="nav__option">
+                  Projects
+                </h2>
+              </a>
+              <a href="#contact" className="nav__option-container">
+                <img src={send} alt="send-icon" />
+                <h2 href="#contact" className="nav__option">
+                  Contact
+                </h2>
+              </a>
+            </div>
           </div>
+          <img
+            onClick={toggleNav}
+            className="nav__toggle"
+            src={toggler ? x : menu}
+            alt="menu-icon"
+          />
+        </nav>
+        <div>
+          {toggler ? (
+            <div className="nav__options--toggled">
+              <a
+                href="#about"
+                onClick={toggleNav}
+                className="nav__option-container"
+              >
+                <img src={user} alt="user-icon" />
+                <h2 href="about" className="nav__option">
+                  About
+                </h2>
+              </a>
+              <a
+                href="#projects"
+                onClick={toggleNav}
+                className="nav__option-container"
+              >
+                <img src={folder} alt="folder-icon" />
+                <h2 href="#projects" className="nav__option">
+                  Projects
+                </h2>
+              </a>
+              <a
+                href="#contact"
+                onClick={toggleNav}
+                className="nav__option-container"
+              >
+                <img src={send} alt="send-icon" />
+                <h2 href="#contact" className="nav__option">
+                  Contact
+                </h2>
+              </a>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
-        <img onClick={toggleNav} className='nav__toggle' src={menu} alt='menu-icon' />
-      </nav>
-      <div>
-        {toggler ?
-          <div className='nav__options--toggled'>
-          <a href='#about' className='nav__option-container'><img src={user} alt='user-icon'/><h2 href='about' className='nav__option'>About</h2></a>
-          <a href='#projects'className='nav__option-container'><img src={folder} alt='folder-icon' /><h2 href='#projects' className='nav__option'>Projects</h2></a>
-          <a href='#contact' className='nav__option-container'><img src={send} alt='send-icon' /><h2 href='#contact' className='nav__option'>Contact</h2></a>
-        </div>:<div></div>}
       </div>
-    </div>
+    </>
   );
 }
 
